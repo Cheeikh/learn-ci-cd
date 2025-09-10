bebc81c68499c06835f24383747320c802a33336
Cheeikh <cheeikhmbacke@gmail.com>
2025-09-10 20:54:42 +0000

    fix

--- src/counter.ts
+++ src/counter.ts
@@ -5,7 +5,6 @@ export function setupCounter(element: HTMLButtonElement) {
     element.innerHTML = `count is ${counter}`
   }
   console.log('Ca marche')
-
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
 }
